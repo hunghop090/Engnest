9bfd4789670434b0521dd5c8cb7c3ea493f2b2c0
nguyen hung <hunghop0909@gmail.com>
2019-05-28 17:48:03 +0700

    chat

--- Engnest/ChatHub.cs
+++ Engnest/ChatHub.cs
@@ -9,29 +9,52 @@ using System.Web;
 using Engnest.Controllers;
 using Engnest.Entities.Common;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace Engnest
 {
     public class ChatHub : Hub
     {
-    public override Task OnConnected()
-    {
-		Section.Add(Constant.USER_SESSION, user.ID);
-		BaseController.HubUser.HubId = Context.ConnectionId;
-        return base.OnConnected();
-    }
+		public class MessageDetail
+		{
+			public string UserName { get; set; }
 
-    public override Task OnDisconnected(bool stopCalled)
-    {
-        return base.OnDisconnected(stopCalled);
-    }
+			public string Message { get; set; }
+		}
+		public class UserDetail
+		{
+			public string ConnectionId { get; set; }
+			public string UserName { get; set; }
+		}
+        static List<UserDetail> ConnectedUsers = new List<UserDetail>();
+        static List<MessageDetail> CurrentMessage = new List<MessageDetail>();
 
-    public void Send(string name, string message)
-    {
-		var ids = name.Split(',').ToList();
-		if(ids.Contains(BaseController.HubUser.UserId.ToString()))
-			Clients.Client(Context.ConnectionId).addNewMessageToPage(name,message);
-    }
+		public void SendChatMessage(string who, string message)
+        {
+			if(who != string.Empty || message != string.Empty)
+			{
+				var User = ConnectedUsers.Where(x=>x.UserName == who).FirstOrDefault();
+				Clients.Client(User.ConnectionId).addChatMessage(message);
+			}
+        }
 
+        public void Connected(string who)
+        {
+			if(who != string.Empty)
+			{
+				UserDetail User = ConnectedUsers.Where(x=>x.UserName == who).FirstOrDefault();
+				if(User == null || User.ConnectionId == Context.ConnectionId)
+				{
+					ConnectedUsers.Remove(User);
+					ConnectedUsers.Add(new UserDetail{ConnectionId = Context.ConnectionId,UserName = who});
+				}
+			}
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            ConnectedUsers.Remove(ConnectedUsers.Where(x=>x.ConnectionId == Context.ConnectionId).FirstOrDefault());
+            return base.OnDisconnected(stopCalled);
+        }
     }
 }
